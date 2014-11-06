9f63387ef096b52a61247ab42a2362df42539f82
Celma Patricia Núñez Espada <PattyNunez@MacBook-de-Celma.local>
2014-11-06 13:18:34 -0600

    fix

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 
 SRC=$(PWD)/src/
 EBIN=$(PWD)/ebin/
-
+INCLUDE=$(PWD)/include/
 all: compile 
 
 compile: 
-	erlc -o $(EBIN)  $(SRC)*.erl
+	erlc -o $(EBIN) -I $(INCLUDE) $(SRC)*.erl
 
 clean:
 	@rm -rf $(EBIN)*.beam
--- src/thiago_code.erl
+++ src/thiago_code.erl
@@ -1,9 +1,13 @@
 -module(thiago_code).
 
 -export([create/1, update/1, delete/1, get/1 ]).
+-include("thiago.hrl").
 
-create(Args) ->
-	io:format("create ~p~n", [Args]).
+create({Name, Data}) ->
+	io:format("create ~p~n", [?PATH]),
+	NewPath = ?PATH ++ "/" ++ Name,
+	{ok,Name} = dets:open_file(Name,[{file, NewPath},{type, duplicate_bag}]),
+	ok =dets:insert(NewPath, [{bucket, key}, {notification, [key1, key2, key3]}]).	
 update(Args) ->
         io:format("update ~p~n", [Args]).
 delete(Args) ->
