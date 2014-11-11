9b83d393e09422788fec4775e6785bf33d846092
Celma Patricia Núñez Espada <PattyNunez@MacBook-de-Celma.local>
2014-11-11 14:13:02 -0600

    Create funcitons insert delete all get

--- src/thiago_code.erl
+++ src/thiago_code.erl
@@ -5,12 +5,27 @@
 
 create({Name, Data}) ->
 	io:format("create ~p~n", [?PATH]),
-	NewPath = ?PATH ++ "/" ++ Name,
+	NewPath = ?PATH ++ "/" ++ Name ++ ".db",
 	{ok,Name} = dets:open_file(Name,[{file, NewPath},{type, duplicate_bag}]),
-	ok =dets:insert(NewPath, [{bucket, key}, {notification, [key1, key2, key3]}]).	
+	ok = dets:insert( Name, [{bucket, key}, {notification, [key1, key2, key3]}]),
+	ok = dets:close( Name ). 
+	
 update(Args) ->
         io:format("update ~p~n", [Args]).
-delete(Args) ->
-        io:format("delete ~p~n", [Args]).
-get(Args) ->
-        io:format("get  ~p~n", [Args]).
+
+delete({all, Name}) ->
+	NewPath = ?PATH ++ "/" ++ Name ++ ".db",
+        {ok, Name}  = dets:open_file(Name,[{file, NewPath},{type, duplicate_bag}]),
+	dets:delete_all_objects(Name);
+
+delete({Name, Key}) ->
+        NewPath = ?PATH ++ "/" ++ Name ++ ".db",
+        {ok, Name}  = dets:open_file(Name,[{file, NewPath},{type, duplicate_bag}]),
+        dets:delete(Name, Key).
+
+get({Name, Bucket}) ->
+	NewPath = ?PATH ++ "/" ++ Name ++ ".db",
+        {ok, Name}  = dets:open_file(Name,[{file, NewPath},{type, duplicate_bag}]),
+	Data = dets:lookup(Name, bucket),
+	io:format("reciving data: ~p~n", [Data]),
+	ok = dets:close( Name ).
